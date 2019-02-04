2eab7f0515ac84d1e22fc7974f86c6fc3a22379a
ATBull81 <45053146+ATBull81@users.noreply.github.com>
2019-02-04 13:13:39 -0600

    Add files via upload

---
+++ Maze/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Maze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxButton = new System.Windows.Forms.Button();
+            this.uxMazePicture = new Ksu.Cis300.MazeLibrary.Maze();
+            this.SuspendLayout();
+            // 
+            // uxButton
+            // 
+            this.uxButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.uxButton.AutoSize = true;
+            this.uxButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.uxButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxButton.Location = new System.Drawing.Point(53, 218);
+            this.uxButton.Name = "uxButton";
+            this.uxButton.Size = new System.Drawing.Size(106, 30);
+            this.uxButton.TabIndex = 0;
+            this.uxButton.Text = "New Maze";
+            this.uxButton.UseVisualStyleBackColor = true;
+            this.uxButton.Click += new System.EventHandler(this.uxButton_Click);
+            // 
+            // uxMazePicture
+            // 
+            this.uxMazePicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxMazePicture.AutoSize = true;
+            this.uxMazePicture.Location = new System.Drawing.Point(13, 11);
+            this.uxMazePicture.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.uxMazePicture.Name = "uxMazePicture";
+            this.uxMazePicture.PathColor = System.Drawing.SystemColors.Highlight;
+            this.uxMazePicture.Size = new System.Drawing.Size(184, 191);
+            this.uxMazePicture.TabIndex = 1;
+            this.uxMazePicture.MouseClick += new System.Windows.Forms.MouseEventHandler(this.uxMazePicture_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(213, 260);
+            this.Controls.Add(this.uxMazePicture);
+            this.Controls.Add(this.uxButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uxButton;
+        private Ksu.Cis300.MazeLibrary.Maze uxMazePicture;
+    }
+}
+
---
+++ Maze/Form1.cs
@@ -0,0 +1,124 @@
+﻿/* Written by: Alec Turnbull
+ * 2/10/16
+ *
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ksu.Cis300.MazeLibrary;
+
+namespace Maze
+{
+    public partial class Form1 : Form
+    {
+        int rows;
+        int cols;
+        bool cellCheck;
+        bool stepCheck;
+        Point firstPoint;
+        Cell curCell = new Cell();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This method reacts to when the user presses the new maze button.
+        /// It resizes the maze to fit the program, and randomizes a new maze.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxButton_Click(object sender, EventArgs e)
+        {
+            uxMazePicture.Generate();
+        }
+        /// <summary>
+        /// This method solves the maze through reverse recursion.
+        /// It goes through each direction and makes a decision.
+        /// </summary>
+        /// <param name="x">the cell containing a set of x and y coordinates</param>
+        public void uxMaze(Cell x)
+        {
+            Stack<Direction> _direct = new Stack<Direction>();
+            cols = uxMazePicture.MazeWidth;
+            rows = uxMazePicture.MazeHeight;
+            bool[,] _a = new bool[rows, cols];
+            curCell = x;
+            _a[curCell.Row, curCell.Column] = true;
+            Direction n = new Direction();
+            n = Direction.North;
+
+
+            while (uxMazePicture.IsInMaze(curCell))
+            {
+                cellCheck = uxMazePicture.IsInMaze(curCell);
+                if (n <= Direction.West)
+                {
+                    stepCheck = uxMazePicture.IsClear(curCell, n);
+                    if (uxMazePicture.IsClear(curCell, n) && ((uxMazePicture.IsInMaze(uxMazePicture.Step(curCell, n)) == false || _a[uxMazePicture.Step(curCell, n).Row, uxMazePicture.Step(curCell, n).Column] == false)))
+                    {
+                        uxMazePicture.DrawPath(curCell, n);
+                        _a[curCell.Row, curCell.Column] = true;
+                        curCell = uxMazePicture.Step(curCell, n);
+                        _direct.Push(n);
+                        n = Direction.North;
+
+                    }
+                    else
+                    {
+                        if (n <= Direction.West)
+                        {
+                            n++;
+                        }
+                    }
+                }
+                if (n > Direction.West && _direct.Count != 0)
+                {
+                    n = _direct.Pop();
+                    curCell = uxMazePicture.ReverseStep(curCell, n);
+                    uxMazePicture.ErasePath(curCell, n);
+                    n++;
+                }
+
+                if (n > Direction.West && _direct.Count == 0)
+                {
+                    MessageBox.Show("No path found.");
+                    break;
+                }
+            }
+        }
+        /// <summary>
+        /// This method collects the location of the mouse click, 
+        /// sending it over to the maze solver.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxMazePicture_MouseClick(object sender, MouseEventArgs e)
+        {
+            firstPoint = e.Location;
+            curCell = uxMazePicture.GetCellFromPixel(firstPoint);
+            bool check = uxMazePicture.IsInMaze(curCell);
+            if (check)
+            {
+                uxMazePicture.EraseAllPaths();
+                uxMaze(curCell);
+                uxMazePicture.Invalidate();
+                check = false;
+            }
+        }
+    }
+
+
+
+
+    }
+
